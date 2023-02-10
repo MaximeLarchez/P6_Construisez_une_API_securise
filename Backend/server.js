20e2df35d47563075c5067097aa66b19c4ebd5be
Maxime <maxime.larchez@orange.fr>
2023-02-10 16:52:55 +0100

    end-projet

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,7 @@
-// //Importe le package Http de node 
-// const http = require('http');
-// // Importation du app.js
-// const app = require('./app');
-
-
-// // permet de dire a l'application sur quelle PORT elle va fonctionné
-// app.set('port', process.env.PORT || 3000);
-
-// // variable qui crée un serveur 
-// const server = http.createServer(app);
-
-// // Ecoute du server sur le PORT par defaut ou 3000
-// server.listen( process.env.PORT || 3000);
-
-
-
-
-
+//Importe le package Http de node 
 const http = require('http');
+// Importation du app.js
 const app = require('./app');
-
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
